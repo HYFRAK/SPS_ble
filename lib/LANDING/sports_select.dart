db04fdd13834918e1f17d4988f6e3217c6a9dba4
HYFRAK <ashishhegde1234@gmail.com>
2022-02-18 12:11:16 +0530

    Initial

--- lib/LANDING/sports_select.dart
+++ lib/LANDING/sports_select.dart
@@ -8,7 +8,9 @@ class SportSelect extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: CommonAppbar(texts: 'SPS',),
+        title: CommonAppbar(
+          texts: 'SPS',
+        ),
       ),
       body: GridView.count(
         padding: EdgeInsets.only(left: 15, right: 15),
@@ -17,7 +19,7 @@ class SportSelect extends StatelessWidget {
           Text("Warm up mode"),
           Text("Racket Sports"),
           Text("Cricket"),
-          Text("Custom")
+          Text("Custo")
         ],
       ),
     );
