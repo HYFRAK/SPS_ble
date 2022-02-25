f6cfee48cbc0a11f4c6ed295aa4c6b67de810486
HYFRAK <ashishhegde1234@gmail.com>
2022-02-25 10:33:32 +0530

    Profile Page added, UI Updated

--- lib/COMMON_ASSETS/sports_select_assets.dart
+++ lib/COMMON_ASSETS/sports_select_assets.dart
@@ -4,56 +4,60 @@ import 'package:sps_ble/COMMON_ASSETS/styles.dart';
 
 class SportsSelectBox extends StatelessWidget {
   final String text, url, info;
+  final Widget x;
   const SportsSelectBox(
-      {Key? key, required this.text, required this.url, required this.info})
+      {Key? key, required this.text, required this.url, required this.info, required this.x})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 15, bottom: 30),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Flexible(
-            child: Container(
-              height: 100,
-              width: 150,
-              child: CachedNetworkImage(
-                imageUrl: url,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => x)) ,
+      child: Container(
+        padding: EdgeInsets.only(left: 15, bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Flexible(
+              child: Container(
+                height: 100,
+                width: 150,
+                child: CachedNetworkImage(
+                  imageUrl: url,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 7,
-          ),
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  text,
-                  style: Head2Style,
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                Text(
-                  info,
-                  style: Body2Style,
-                )
-              ],
+            SizedBox(
+              width: 7,
             ),
-          ),
-          SizedBox(
-            width: 6,
-          ),
-          Icon(
-            Icons.arrow_forward_ios_sharp,
-            color: Colors.white,
-          )
-        ],
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    text,
+                    style: Head2Style,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    info,
+                    style: Body2Style,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 6,
+            ),
+            Icon(
+              Icons.arrow_forward_ios_sharp,
+              color: Colors.white,
+            )
+          ],
+        ),
       ),
     );
   }
